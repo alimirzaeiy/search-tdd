2dc6672f1f647c225000415bb619cac65579bba5
ali mirzaei <a.mirzaei@mohaymen.ir>
2022-12-31 16:51:30 +0330

    Try to fix build

--- SearchTDD/MapOfIndexedWordsByFile.cs
+++ SearchTDD/MapOfIndexedWordsByFile.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit.Sdk;
-
-namespace search_tdd
+﻿namespace search_tdd
 {
     public class MapOfIndexedWordsByFile
     {
