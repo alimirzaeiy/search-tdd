4f02958ae8d89cceff83dac7e6f6401d8f7351ca
ali mirzaei <a.mirzaei@mohaymen.ir>
2022-12-31 16:19:01 +0330

    Try to fix build

--- SearchTDD.Tests/indexingTests.cs
+++ SearchTDD.Tests/indexingTests.cs
@@ -1,12 +1,4 @@
-﻿using FluentAssertions;
-using search_tdd;
-using search_tddTests4;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using search_tddTests4;
 using Xunit;
 using Xunit.Abstractions;
 
